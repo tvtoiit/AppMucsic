58d00979fa4d7554e3f360f68dd89541970fe1aa
vantoicntt0@gmail.com <TranVanToi234>
2024-10-30 06:47:44 +0700

    edit patch favorite

--- app/src/main/java/com/example/appmusic/activity/Tab_Favorite_Fragment.java
+++ app/src/main/java/com/example/appmusic/activity/Tab_Favorite_Fragment.java
@@ -61,6 +61,6 @@ public class Tab_Favorite_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_dowload, container, false);
+        return inflater.inflate(R.layout.fragment_favorite, container, false);
     }
 }
