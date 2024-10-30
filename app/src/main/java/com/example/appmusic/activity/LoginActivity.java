86f109227a8b5564ed0083f1c3071cd4fc32fcb4
vantoicntt0@gmail.com <TranVanToi234>
2024-10-30 19:04:07 +0700

    delete login

---
+++ app/src/main/java/com/example/appmusic/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.appmusic.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appthibanglaixe.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/appmusic/activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.appmusic.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.appthibanglaixe.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
